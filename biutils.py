5cfc64e0767cd64f10fc4af7ba76090d45ee4d03
Sergei Bugrov <sergei.v.bugrov@gmail.com>
2016-07-06 17:23:27 -0600

    rna_to_aa(ran) added

--- biutils.py
+++ biutils.py
@@ -1,5 +1,24 @@
 from itertools import product
 
+def rna_to_aa(rna):
+  ''' Translate an RNA string into an amino acid string.
+      Input: An RNA string Pattern.
+      Output: The translation of Pattern into an amino acid string Peptide.
+  '''
+  rna_codon_table = { "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L", "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+                      "UAU":"Y", "UAC":"Y", "UAA":"*", "UAG":"*", "UGU":"C", "UGC":"C", "UGA":"*", "UGG":"W",
+                      "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L", "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+                      "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q", "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+                      "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M", "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+                      "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K", "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+                      "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V", "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+                      "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E", "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
+  
+  rna = rna.upper()
+  rna = [rna[i:3+i] for i in range(0, len(rna), 3)]
+    
+  return ''.join([rna_codon_table[codon] for codon in rna])
+
 def all_kmers(k):
   '''
   return a list of all possible k-mers
